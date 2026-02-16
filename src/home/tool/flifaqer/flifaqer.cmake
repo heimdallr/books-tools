49d92c9c6a784171a47a8698d4e6e3667c8efcea
heimdallr <heimdalrnsk@gmail.com>
2026-02-16 16:18:40 +0700

    #40 Добавлена иконка

--- src/home/tool/flifaqer/flifaqer.cmake
+++ src/home/tool/flifaqer/flifaqer.cmake
@@ -1,5 +1,14 @@
+CreateWinRC(app
+    	COMPANY_NAME      "HomeCompa"
+    	FILE_NAME         "flifaqer"
+    	FILE_DESCRIPTION  "FAQ translation util"
+    	APP_ICON          "${CMAKE_CURRENT_LIST_DIR}/resources/icons/main.ico"
+    	APP_VERSION       ${PRODUCT_VERSION}
+)
+
 AddTarget(flifaqer	app
 	PROJECT_GROUP App
+	WIN_RC ${CMAKE_BINARY_DIR}/resources/app.rc
 	SOURCE_DIRECTORY
 		"${CMAKE_CURRENT_LIST_DIR}"
 	LINK_LIBRARIES
