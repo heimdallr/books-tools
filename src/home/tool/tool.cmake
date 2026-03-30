a1805e40280bcd3bc6a9c266c0376fc1fc28d491
heimdallr <heimdallrnsk@gmail.com>
2026-03-30 18:44:29 +0700

    Мелкие Линукс-правки

--- src/home/tool/tool.cmake
+++ src/home/tool/tool.cmake
@@ -1,17 +1,19 @@
 #Да, колхоз. Но я устал
-set(D)
-if (${CMAKE_BUILD_TYPE} STREQUAL "Debug")
-	set(D d)
-endif()
+if (MSVC)
+	set(D)
+	if (${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+		set(D d)
+	endif()
 
-set(QT_BIN_FILES
-	${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Core${D}.dll
-	${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Gui${D}.dll
-	${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Network${D}.dll
-	${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Svg${D}.dll
-	${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Widgets${D}.dll
-)
-file(COPY ${QT_BIN_FILES} DESTINATION ${CMAKE_BINARY_DIR}/bin)
+	set(QT_BIN_FILES
+		${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Core${D}.dll
+		${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Gui${D}.dll
+		${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Network${D}.dll
+		${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Svg${D}.dll
+		${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Widgets${D}.dll
+	)
+	file(COPY ${QT_BIN_FILES} DESTINATION ${CMAKE_BINARY_DIR}/bin)
+endif()
 
 file(GLOB sources_list LIST_DIRECTORIES true "${CMAKE_CURRENT_LIST_DIR}/*")
 foreach(dir ${sources_list})
