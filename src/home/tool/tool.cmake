f9ea6787493d70a5ae818b0578739a0b9fe7751b
heimdallr <heimdalrnsk@gmail.com>
2026-03-31 10:55:02 +0700

    deploy scripts

--- src/home/tool/tool.cmake
+++ src/home/tool/tool.cmake
@@ -13,6 +13,14 @@ if (MSVC)
 		${QT6_INSTALL_PREFIX}/${QT6_INSTALL_BINS}/Qt6Widgets${D}.dll
 	)
 	file(COPY ${QT_BIN_FILES} DESTINATION ${CMAKE_BINARY_DIR}/bin)
+
+	if (${CMAKE_BUILD_TYPE} STREQUAL "Release")
+		install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/imageformats DESTINATION .)
+		install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/platforms DESTINATION .)
+		install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/styles DESTINATION .)
+		install(DIRECTORY ${CMAKE_BINARY_DIR}/bin/tls DESTINATION .)
+		install(FILES ${QT_BIN_FILES} DESTINATION .)
+	endif()
 endif()
 
 file(GLOB sources_list LIST_DIRECTORIES true "${CMAKE_CURRENT_LIST_DIR}/*")
