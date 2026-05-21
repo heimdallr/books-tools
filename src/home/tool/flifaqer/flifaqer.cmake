15e927411bed7f4d622a0db6f693dda64b39a1d2
heimdallr <heimdalrnsk@gmail.com>
2026-05-21 19:56:29 +0700

    А проставим-ка всем файловую инфу

--- src/home/tool/flifaqer/flifaqer.cmake
+++ src/home/tool/flifaqer/flifaqer.cmake
@@ -1,14 +1,5 @@
-CreateWinRC(app
-    	COMPANY_NAME      "HomeCompa"
-    	FILE_NAME         "flifaqer"
-    	FILE_DESCRIPTION  "FAQ translation util"
-    	APP_ICON          "${CMAKE_CURRENT_LIST_DIR}/resources/icons/main.ico"
-    	APP_VERSION       ${PRODUCT_VERSION}
-)
-
 AddTarget(fliFAQer	app
 	PROJECT_GROUP App
-	WIN_RC ${CMAKE_BINARY_DIR}/resources/app.rc
 	SOURCE_DIRECTORY
 		"${CMAKE_CURRENT_LIST_DIR}"
 	LINK_LIBRARIES
